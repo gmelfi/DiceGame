8be1c2a66fbaad468fefbd562e462539a3b7ae33
logonrm <logonrm@fiap.com.br>
2018-04-20 22:30:01 -0300

    Initial commit

---
+++ app/src/main/java/com/dice/melfalho/dicegame/GameActivity.kt
@@ -0,0 +1,14 @@
+package com.dice.melfalho.dicegame
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+
+
+}
